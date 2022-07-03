3e20c6a79d62dfae0de5c8bfaf4b434032625088
Takahisa99 <106425553+Takahisa99@users.noreply.github.com>
2022-07-03 09:37:53 +0000

    フォロー、フォロワー機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -6,12 +6,19 @@ Rails.application.routes.draw do
   root to: "homes#top"
   get  "home/about"=>"homes#about", as: "about"
 
+
+
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update]do
    resources :book_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
-  end
-  resources :users, only: [:index,:show,:edit,:update]
 
+
+  end
+  resources :users, only: [:index,:show,:edit,:update] do
+   resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+ end
 
 
 
